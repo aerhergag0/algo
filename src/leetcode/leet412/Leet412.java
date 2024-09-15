3e6e97f4934a32e30126c735d0762b166ee623b2
aerhergag0 <aerhergag@naver.com>
2024-09-15 17:13:01 +0900

    Leet412

---
+++ src/leetcode/leet412/Leet412.java
@@ -0,0 +1,32 @@
+package leetcode.leet412;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leet412 {
+	public List<String> fizzBuzz(int n) {
+		List<String> answer = new ArrayList<String>();
+
+		for (int i=1; i<=n; i++) {
+			if (isFizz(i) && isBuzz(i)) {
+				answer.add("FizzBuzz");
+			} else if (isFizz(i) && !isBuzz(i)) {
+				answer.add("Fizz");
+			} else if (!isFizz(i) && isBuzz(i)) {
+				answer.add("Buzz");
+			} else {
+				answer.add(String.valueOf(i));
+			}
+		}
+
+		return answer;
+	}
+
+	private boolean isFizz(int i) {
+		return i % 3 == 0;
+	}
+
+	private boolean isBuzz(int i) {
+		return i % 5 == 0;
+	}
+}
