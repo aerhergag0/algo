d45524c2f68d7e767554358a1cd86e3c7d51dc7a
aerhergag0 <aerhergag@naver.com>
2024-09-16 17:25:58 +0900

    Leet383

---
+++ src/leetcode/leet383/Leet383.java
@@ -0,0 +1,26 @@
+package leetcode.leet383;
+
+import java.util.HashMap;
+
+public class Leet383 {
+	public boolean canConstruct(String ransomNote, String magazine) {
+
+		HashMap<Character, Integer> magazineMap = new HashMap<>();
+
+		for (char c : magazine.toCharArray()) {
+			int currentCount = magazineMap.getOrDefault(c, 0);
+			magazineMap.put(c, currentCount + 1);
+		}
+
+		for (char r : ransomNote.toCharArray()) {
+			if (!magazineMap.containsKey(r) || magazineMap.getOrDefault(r, 0) == 0) {
+				return false;
+			}
+
+			int currentCount = magazineMap.getOrDefault(r, 0);
+			magazineMap.put(r, currentCount - 1);
+		}
+
+		return true;
+	}
+}
