f1a1cce4d21e4e37d8558fdea3decab64117bffa
aerhergag0 <aerhergag@naver.com>
2024-09-26 14:54:29 +0900

    Leet933

---
+++ src/leetcode/queue/leet933/Leet933.java
@@ -0,0 +1,26 @@
+package leetcode.queue.leet933;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Leet933 {
+
+	Queue<Integer> q;
+
+	public void RecentCounter() {
+		q = new LinkedList();
+	}
+
+	public int ping(int t) {
+		q.add(t);
+		while (q.peek() < t - 3000)
+			q.poll();
+		return q.size();
+	}
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
