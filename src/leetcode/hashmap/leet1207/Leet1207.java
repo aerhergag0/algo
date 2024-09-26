be9f9955b4e10e282743beca3316960e3fc631a3
aerhergag0 <aerhergag@naver.com>
2024-09-26 13:49:28 +0900

    Leet1207

---
+++ src/leetcode/hashmap/leet1207/Leet1207.java
@@ -0,0 +1,26 @@
+package leetcode.hashmap.leet1207;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Leet1207 {
+	public boolean uniqueOccurrences(int[] arr) {
+		HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+		for (int i : arr) {
+			hashMap.put(i, hashMap.getOrDefault(i, 0) + 1);
+		}
+
+		HashSet<Integer> hashSet = new HashSet<>();
+
+		for (int i : hashMap.values()) {
+			if (hashSet.contains(i)) {
+				return false;
+			} else {
+				hashSet.add(i);
+			}
+		}
+
+		return true;
+	}
+}
