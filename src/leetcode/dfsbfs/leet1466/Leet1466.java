d72617966db3e4448ac76e4aa867297ff4f1e801
aerhergag0 <aerhergag@naver.com>
2024-10-05 17:51:25 +0900

    on 1466

--- src/leetcode/dfsbfs/leet1466/Leet1466.java
+++ src/leetcode/dfsbfs/leet1466/Leet1466.java
@@ -1,17 +1,44 @@
 package leetcode.dfsbfs.leet1466;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Leet1466 {
+
+	int answer = 0;
+
 	public int minReorder(int n, int[][] connections) {
-		int[][] graph = new int[n][n];
+		List<List<int[]>> graph = new ArrayList<>();
 
-		for (int i=0; i<n; i++) {
-			int a = connections[i][0];
-			int b = connections[i][1];
+		for (int i = 0; i < n; i++) {
+			graph.add(new ArrayList<>());
+		}
 
-			graph[a][b] = 1;
-			graph[b][a] = 1;
+		for (int[] connection : connections) {
+			graph.get(connection[0]).add(new int[]{connection[1], 1});
+			graph.get(connection[1]).add(new int[]{connection[0], 0});
+		}
+
+		boolean[] visited = new boolean[n];
+		dfs(graph, visited, 0);
+
+		return answer;
+	}
+
+	private void dfs(List<List<int[]>> graph, boolean[] visited, int currentCity) {
+		visited[currentCity] = true;
+
+		for (int[] neighbor : graph.get(currentCity)) {
+			int nextCity = neighbor[0];
+			int direction = neighbor[1];
+
+			if (!visited[nextCity]) {
+				if (direction == 1) {
+					answer++;
+				}
+				dfs(graph, visited, nextCity);
+			}
 		}
 	}
 
-	private
 }
