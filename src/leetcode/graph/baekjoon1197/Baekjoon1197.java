6bd06f872c2f93ade71e2a3a08217b5e53c17d33
aerhergag0 <aerhergag@naver.com>
2024-10-17 20:05:45 +0900

    Baekjoon1197

---
+++ src/leetcode/graph/baekjoon1197/Baekjoon1197.java
@@ -0,0 +1,16 @@
+package leetcode.graph.baekjoon1197;
+
+import java.io.*;
+
+public class Baekjoon1197 {
+
+	static int V, E;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+
+	}
+}
